13e223870a349148ee84e3e8dafcc678c19e9129
Colleen Qiu <pikachill@outlook.com>
2020-08-24 00:54:05 -0700

    Changed the input directions to avoid BCT bugs and changed the call for item 4

--- query.py
+++ query.py
@@ -1,5 +1,5 @@
 # time format example:2020-07-02T00:00:00
-
+#all directions: 'ne,ns,nw,es,ew,en,sw,sn,se,wn,we,ws,nrl,nlr,erl,elr,srl,slr,wrl,wlr,'
 import json
 import requests
 from tkinter import filedialog
@@ -14,9 +14,8 @@ def mode1_query(udid, d1, d2, threshold, print_only):
     # Daily API Request
     start_date = d1 + 'T00:00:00'
     end_date = d2 + 'T00:00:00'
-    print(start_date)
 
-    daily_request = API_Connect(udid, start_date, end_date, 'ne,ns,nw,es,ew,en,sw,sn,se,wn,we,ws,nrl,nlr,erl,elr,srl,slr,wrl,wlr,',
+    daily_request = API_Connect(udid, start_date, end_date, 'ne,ns,nw,es,ew',
                                 '3')
     query = daily_request.api_request()  # dictionary containing API call for daily aggregation
     # print(json.dumps(query,indent=2))
@@ -27,8 +26,8 @@ def mode1_query(udid, d1, d2, threshold, print_only):
 
     for i in range(len(list_peak_times) - 1):
         count = i + 1
-        call = API_Connect(udid.get(), list_peak_times[i], list_peak_times[count],
-                           'ne,ns,nw,es,ew,en,sw,sn,se,wn,we,ws,nrl,nlr,erl,elr,srl,slr,wrl,wlr,', '2').api_request()
+        call = API_Connect(udid, list_peak_times[i], list_peak_times[count],
+                           'ne,ns,nw,es,ew', '2').api_request()
         hourly_query_list.append(call)  # list containing hourly aggregation for all the days
 
     # Assigning values to analytics 1-11 #
@@ -51,7 +50,7 @@ def mode1_query(udid, d1, d2, threshold, print_only):
 
         top_ped_peak_hours = daily_request.sim_peak_hours(index, True)
         report_peak_hours_ped.append(top_ped_peak_hours)
-        report_threshold_hours_ped[date] = daily_request.sim_count_hour_above_threshold(index, threshold.get(), True)
+        report_threshold_hours_ped[date] = daily_request.sim_count_hour_above_threshold(index, threshold, True)
 
     # 2 Daily Approach Flow
     (all_approach, most_used_approach, least_used_approach) = daily_request.sim_peak_approach(query)
@@ -59,8 +58,8 @@ def mode1_query(udid, d1, d2, threshold, print_only):
     # 3 Daily Lane Flow
     (daily_lane_flow, most_used_lane, least_used_lane) = daily_request.sim_lane_sum(query)
 
-    # 4 Daily Average Total Flow
-    daily_average = daily_request.sim_avg_daily_traffic_flow(query)
+    # 4 Daily Average Total Traffic Flow
+    daily_average = daily_request.sim_avg_daily_traffic_flow(daily_total)
 
     # 5 Weekday Average Approach Flow
     weekday_flow = daily_request.sim_weekday_flow(query)
@@ -89,7 +88,7 @@ def mode1_query(udid, d1, d2, threshold, print_only):
     print("Average daily traffic flow:")
     print(daily_average)
 
-    print("Average weekday daily traffic & pedestrian flow: ")
+    print("Average weekday daily traffic flow: ")
     print(json.dumps(weekday_flow, indent=2))
 
     print("No. of hour above threshold for each day: ")
@@ -106,12 +105,12 @@ def mode1_query(udid, d1, d2, threshold, print_only):
     print(json.dumps(least_used_crosswalk, indent=2))
 
     # csv export #
-    if print_only == False:
-        export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
-        analyzed_data = {}
-        analyzed_data['Daily Sum'] = daily_total
-        analyzed_data['Ped Daily Sum'] = daily_total_ped
-        daily_request.convertToCSV(analyzed_data, export_file_path)
+    # if print_only == False:
+    #     export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
+    #     analyzed_data = {}
+    #     analyzed_data['Daily Sum'] = daily_total
+    #     analyzed_data['Ped Daily Sum'] = daily_total_ped
+    #     daily_request.convertToCSV(analyzed_data, export_file_path)
 
 
 #####Mode 2 Query######
