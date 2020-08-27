42120162e963b87768d27584c9e43df593a86b3f
Colleen Qiu <pikachill@outlook.com>
2020-08-27 11:24:45 -0700

    Added error detection and print statements

--- ui_execution.py
+++ ui_execution.py
@@ -16,8 +16,27 @@ if __name__ == "__main__":
     (udid, mode, d1, t1, d2, t2, threshold, a, f, file_path_name) = pull_val_from_GUI(ui)
     print(udid, mode, d1, t1, d2, t2, threshold, a, f, file_path_name)  # tests if the values has been returned
 
+    #check if the user has input all necessary parameters
+    if udid == 'None':
+        print ("Please select an intersection.")
+    if mode == 'None':
+        print ("Please select a data mode.")
+    if d1 == 'None':
+        print ("Please enter a start date.")
+    if d2 == 'None':
+        print ("Please enter an end date.")
+    if mode == 2 and t1 == 'None':
+        print ("Print enter a start time.")
+    if mode == 2 and t2 == 'None':
+        print ("Print enter an end time.")
+    if a == 'None':
+        print ("Please select an aggregation type.")
+    if mode == 2 and f == 'None':
+        print ("Please select directions.")
+
     # executes query functions based on the selected mode
-    if mode == 1:
-        mode1_query(udid, d1, d2, threshold, file_path_name)
-    elif mode == 2:
-        mode2_query(udid, mode, d1, t1, d2, t2, a, f, file_path_name)
+    if udid!= 'None' and mode != 'None' and d1 != 'None' and d2 != 'None' and a!= 'None'and f != 'None':
+        if mode == 1:
+            mode1_query(udid, d1, d2, threshold, file_path_name)
+        elif mode == 2 and t1!= 'None' and t2 != 'None':
+            mode2_query(udid, mode, d1, t1, d2, t2, a, f, file_path_name)
